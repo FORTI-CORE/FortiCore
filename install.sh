111ceaa75460659718a06fdc3b75c402fd0206ad
princebabou <baboumanzi69@gmail.com>
2025-02-04 21:03:11 +0200

    Made venv in install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,18 @@ fi
 # Update package lists
 apt update
 
-# Install basic requirements
-apt install -y python3 python3-pip golang-go figlet
+# Install system dependencies
+apt install -y python3 python3-pip python3-venv golang-go figlet
 
 # Install required pentesting tools
-apt install -y nmap dirb nikto dnsmap hashcat hydra john medusa ncrack netcat sqlmap
+apt install -y nmap dirb nikto dnsmap hashcat hydra john medusa ncrack netcat sqlmap subfinder amass
+
+# Create and activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install Python packages in virtual environment
+pip3 install -r requirements.txt
 
 # Install Go-based tools
 echo "Installing Go-based security tools..."
@@ -23,24 +30,25 @@ GO111MODULE=on go install -v github.com/tomnomnom/assetfinder@latest
 GO111MODULE=on go install -v github.com/ffuf/ffuf@latest
 GO111MODULE=on go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 
-# Install additional tools
-apt install -y subfinder amass
-
-# Install the Python package
-pip3 install -e .
-
-# Install Python-based tools
-pip3 install xsstrike
-pip3 install wafw00f
-pip3 install whatweb
-pip3 install -r requirements.txt
-
 # Add Go binaries to PATH
 echo 'export PATH=$PATH:/root/go/bin' >> ~/.bashrc
-source ~/.bashrc
 
-# Make script executable
-chmod +x install.sh
+# Create wrapper script for ftcore
+cat > /usr/local/bin/ftcore << 'EOF'
+#!/bin/bash
+VENV_PATH="$(dirname $(dirname $(readlink -f $0)))/venv"
+source "$VENV_PATH/bin/activate"
+python3 -m forticore "$@"
+EOF
+
+# Make wrapper script executable
+chmod +x /usr/local/bin/ftcore
+
+# Install the package in development mode
+pip3 install -e .
 
 echo "FortiCore has been installed successfully!"
-echo "You can now run it by typing 'ftcore' in your terminal." 
+echo "You can now run it by typing 'ftcore' in your terminal."
+
+# Deactivate virtual environment
+deactivate 
